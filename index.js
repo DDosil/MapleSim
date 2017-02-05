45d3a5956ce8a97ba1b3b8a5fc8bdf0c28274fef
DDosil <skeldk2@naver.com>
2017-02-06 00:19:48 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,15 @@
 			12렙 순피 1205
 			->레벨당 15 상승.
 			*/
+			
+			/*스탯의 종류
+			최종데미지%	데미지%	공격력%
+			공격력 크리티컬데미지 크리티컬
+			스탯 부스탯 HP 공격속도 숙련도
+			방어율무시 보스공격력 데미지%증가 최종데미지%증가
+			*/
+			
+			
 		//클래스 선언:임시
 		function status(){
 			this.att;
@@ -39,38 +48,50 @@
 					return a;
 				}
 			};
+				
 		}
 		var Mystat = new status();
-		
-		
+
 		//키 입력시 갱신
-		document.onkeyup = function(){
-		Mystat.att = parseFloat(document.basestat.att.value);
-		Mystat.mainstat = parseFloat(document.basestat.mainstat.value);
-		Mystat.substat = parseFloat(document.basestat.substat.value);
-		Mystat.attmult = parseFloat(document.mult.att.value);
-		Mystat.mainstatmult = parseFloat(document.mult.mainstat.value)+parseFloat(document.mult.allstat.value);
-		Mystat.substatmult = parseFloat(document.mult.substat.value)+parseFloat(document.mult.allstat.value);
-		Mystat.fixedatt = parseFloat(document.fixed.att.value);
-		Mystat.fixedmainstat = parseFloat(document.fixed.mainstat.value);
-		Mystat.fixedsubstat = parseFloat(document.fixed.substat.value);
-		document.attReport.result.value = Math.ceil(Mystat.total()/5) + " ~ " + Mystat.total();
-		document.reports.totalstat.value = Mystat.statvalue();
-		document.reports.totalatt.value = Mystat.totalatt();
-		};
+		document.onkeyup = function (){
+				//기본 스탯 파싱
+				Mystat.mainstat = parseFloat(document.basestat.mainstat.value) + parseFloat(document.itemstat.mainstat.value);
+				Mystat.substat = parseFloat(document.basestat.substat.value) + parseFloat(document.itemstat.substat.value);
+				//아이템 스탯 파싱
+				Mystat.att = parseFloat(document.itemstat.att.value);
+				Mystat.attmult = parseFloat(document.itemstat.attp.value);
+				Mystat.mainstatmult = parseFloat(document.itemstat.mainstatp.value)+parseFloat(document.itemstat.allstatp.value);
+				Mystat.substatmult = parseFloat(document.itemstat.substatp.value)+parseFloat(document.itemstat.allstatp.value);
+				//고정 스탯 파싱
+				Mystat.fixedatt = parseFloat(document.fixed.att.value);
+				Mystat.fixedmainstat = parseFloat(document.fixed.mainstat.value);
+				Mystat.fixedsubstat = parseFloat(document.fixed.substat.value);
+				document.attReport.result.value = Math.ceil(Mystat.total()/5) + " ~ " + Mystat.total();
+				document.reports.totalstat.value = Mystat.statvalue();
+				document.reports.totalatt.value = Mystat.totalatt();
+				document.reports.d1.value = Mystat.mainstat;
+			};
 		
 		//마우스 클릭시 갱신
-		document.onclick = function(){
-		Mystat.att = parseFloat(document.basestat.att.value);
-		Mystat.mainstat = parseFloat(document.basestat.mainstat.value);
-		Mystat.substat = parseFloat(document.basestat.substat.value);
-		Mystat.attmult = parseFloat(document.mult.att.value);
-		Mystat.mainstatmult = parseFloat(document.mult.mainstat.value)+parseFloat(document.mult.allstat.value);
-		Mystat.substatmult = parseFloat(document.mult.substat.value)+parseFloat(document.mult.allstat.value);
-		Mystat.fixedatt = parseFloat(document.fixed.att.value);
-		Mystat.fixedmainstat = parseFloat(document.fixed.mainstat.value);
-		Mystat.fixedsubstat = parseFloat(document.fixed.substat.value);
-		document.attReport.result.value = Math.ceil(Mystat.total()/5) + " ~ " + Mystat.total();
-		document.reports.totalstat.value = Mystat.statvalue();
-		document.reports.totalatt.value = Mystat.totalatt();
-		};
+		document.onclick = function (){
+				//레벨 파싱
+				//document.basestat.mainstat.value = (document.basestat.lev.value*5)+18;
+				//기본 스탯 파싱
+				Mystat.mainstat = parseFloat(document.basestat.mainstat.value) + parseFloat(document.itemstat.mainstat.value);
+				Mystat.substat = parseFloat(document.basestat.substat.value) + parseFloat(document.itemstat.substat.value);
+				//아이템 스탯 파싱
+				Mystat.att = parseFloat(document.itemstat.att.value);
+				Mystat.attmult = parseFloat(document.itemstat.attp.value);
+				Mystat.mainstatmult = parseFloat(document.itemstat.mainstatp.value)+parseFloat(document.itemstat.allstatp.value);
+				Mystat.substatmult = parseFloat(document.itemstat.substatp.value)+parseFloat(document.itemstat.allstatp.value);
+				Mystat.fixedatt = parseFloat(document.fixed.att.value);
+				Mystat.fixedmainstat = parseFloat(document.fixed.mainstat.value);
+				Mystat.fixedsubstat = parseFloat(document.fixed.substat.value);
+				document.attReport.result.value = Math.ceil(Mystat.total()/5) + " ~ " + Mystat.total();
+				document.reports.totalstat.value = Mystat.statvalue();
+				document.reports.totalatt.value = Mystat.totalatt();
+			};
+		
+		function jobstat(){
+			
+		}
