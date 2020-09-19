1f9e988e7b2465d9ec9a6870b4d205863bb1e9f1
DDosil <skeldk2@naver.com>
2020-09-20 04:13:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -235,7 +235,7 @@ logs+= '<button type="button" class="btn btn-dark btn-lg" onclick="getres()">돌
 
 var allspentmeso = 0;
 var kkkk=100000;
-	if(targetStar==25){kkkk=10000;}else{kkkk=100000;}
+	if(targetStar==25){kkkk=1000;}else{kkkk=100000;}
 	for(var i=0;i<kkkk;i++){
 		spentmeso = 0;
 		myStar = parseInt(document.sf.startstar.value);
